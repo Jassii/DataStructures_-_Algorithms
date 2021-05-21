fa768a2aceaba15c439138e9482e6fb131f9e180
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-05-21 20:57:41 +0530

    Create Heap_Imple_PriorityQueue.java

---
+++ Heap/Heap_Imple_PriorityQueue.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {10,7,11,5,2,13,1,45};
+        Heap h = new Heap(); //creating an object of the Heap class..
+        h.minHeap(arr);  //for the minimum heap..
+        h.maxHeap(arr);  //for the maximum heap..
+    }
+}
+class Heap
+{
+    public void minHeap(int arr[])
+    {
+        //create a prioriy queue..
+        PriorityQueue<Integer> P = new PriorityQueue<>();  //initialized the PriorityQueue.
+        int i;
+        for(i=0;i<arr.length;i++)
+        {
+            P.add(arr[i]);  //insert in the PrioriyQueue.
+            System.out.print(P.peek()+" ");
+        }
+        System.out.println("..AND..");
+        
+        for(i=0;i<arr.length-1;i++)
+        {
+            P.poll();  //removing the value from the PrioriyQueue
+            System.out.print(P.peek()+" ");
+        }
+    }
+    public void maxHeap(int arr[])
+    {
+        System.out.println();
+        PriorityQueue<Integer> P = new PriorityQueue<>(Collections.reverseOrder());  //for maxHeap..
+        int i;
+        for(i=0;i<arr.length;i++)
+        {
+            P.add(arr[i]);
+            System.out.print(P.peek()+" ");
+        }
+        System.out.println();
+        for(i=0;i<arr.length-1;i++)
+        {
+            P.poll();  //remove the top element..
+            System.out.print(P.peek()+" ");
+        }
+    }
+}
