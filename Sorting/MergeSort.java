dfc9e9e476d42d62e5e132444d25e04ae963443d
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-06-14 20:23:38 +0530

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -92,23 +92,6 @@ class MergeSort
 	        System.out.print(arr[i]+" ");
 	    }
 	}
-	
-	
-	public static void arrSort(int arr[],int start,int end)
-	{
-	    for(int i=0;i<arr.length-1;i++)
-	    {
-	        for(int j=0;j<arr.length-i-1;j++)
-	        {
-	            if(arr[j]>arr[j+1])
-	            {
-	                int temp = arr[j];
-	                arr[j] = arr[j+1];
-	                arr[j+1] = temp;
-	           }
-	        }
-	    }
-	}
 	//this will help for divide and conquer..
 	public void sort(int arr[],int l,int r)
 	{
